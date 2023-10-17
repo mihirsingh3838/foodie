bb2ae48ee980cb0ff5bdbe5444bc20c2895f9a76
Mihir Singh <mihirsingh252001@gmail.com>
2023-10-17 12:46:08 +0530

    Persistent Nav bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foodie/pages/home/main_food_page.dart';
 import 'package:foodie/utils/colors.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +13,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
+  late PersistentTabController _controller;
+
   List pages = [
     const MainFoodPage(),
     const Center(child: Text('Next Page')),
@@ -25,25 +29,106 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: AppColors.mainColor,
-        unselectedItemColor: Colors.amberAccent,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        currentIndex: _selectedIndex,
-        selectedFontSize: 0.0,
-        unselectedFontSize: 0.0,
-        onTap: onTapNav,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.archive), label: 'History'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Me'),
-        ],
+  void initState() {
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      const MainFoodPage(),
+      const Center(child: Text('Next Page')),
+      const Center(child: Text('Next next Page')),
+      const Center(child: Text('Next next next Page')),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.home),
+        title: ("Home"),
+        activeColorPrimary: AppColors.mainColor,
+        inactiveColorPrimary: AppColors.iconColor2,
       ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.archive),
+        title: ("Archive"),
+        activeColorPrimary: AppColors.mainColor,
+        inactiveColorPrimary: AppColors.iconColor2,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.shopping_cart),
+        title: ("Cart"),
+        activeColorPrimary: AppColors.mainColor,
+        inactiveColorPrimary: AppColors.iconColor2,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.person),
+        title: ("Me"),
+        activeColorPrimary: AppColors.mainColor,
+        inactiveColorPrimary: AppColors.iconColor2,
+      ),
+    ];
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: pages[_selectedIndex],
+  //     bottomNavigationBar: BottomNavigationBar(
+  //       selectedItemColor: AppColors.mainColor,
+  //       unselectedItemColor: Colors.amberAccent,
+  //       showSelectedLabels: false,
+  //       showUnselectedLabels: false,
+  //       currentIndex: _selectedIndex,
+  //       selectedFontSize: 0.0,
+  //       unselectedFontSize: 0.0,
+  //       onTap: onTapNav,
+  //       items: const [
+  //         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+  //         BottomNavigationBarItem(icon: Icon(Icons.archive), label: 'History'),
+  //         BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+  //         BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Me'),
+  //       ],
+  //     ),
+  //   );
+  //}
+
+  @override
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style12, // Choose the nav bar style with this property.
     );
   }
 }
